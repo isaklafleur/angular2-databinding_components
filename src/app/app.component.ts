5abf633144772c9edb7e5bd56861fec63b53e17e
Isak La Fleur <isak.engdahl@se.atlascopco.com>
2017-02-24 15:44:52 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+      <h1>Root Component</h1>
+      <fa-lifecycle *ngIf="!delete" [bindable]="boundValue">
+        <p #boundContent>{{ test }}</p>
+      </fa-lifecycle>
+
+      <button (click)="delete = true">Click to delete</button>
+      <button (click)="test = 'Changed value'">Click to Change Content</button>
+      <button (click)="boundValue = 2000">Click to Change Binding</button>
+  `
 })
 export class AppComponent {
-  title = 'app works!';
+  delete = false;
+  test = "Starting value";
+  boundValue = 1000;
+
 }
